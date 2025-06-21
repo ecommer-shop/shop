25548edbbe7b5597e691ff103b0a4af96c05cef8
Ronnovic Rigel Toth <t-rrigel@wingo.com>
2025-06-21 02:32:22 -0500

    Refactor populate script to simplify database population logic and improve error handling

--- src/scripts/populate.ts
+++ src/scripts/populate.ts
@@ -1,24 +1,14 @@
-import path from 'path';
-import { bootstrap, defaultConfig, mergeConfig } from '@vendure/core';
+import { bootstrap } from '@vendure/core';
 import { populate } from '@vendure/core/cli';
 import { config } from '../vendure-config';
 import { initialData } from './initial-data';
 
-const populateConfig = mergeConfig(defaultConfig, config);
-
-populate(
-  () =>
-    bootstrap(populateConfig).then((app) => {
-      return app;
-    }),
-  initialData,
-  undefined
-)
-  .then(() => {
-    console.log('Populated DB!');
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+populate(() => bootstrap(config), initialData, undefined)
+  .then((app) => app.close())
+  .then(
+    () => process.exit(0),
+    (err) => {
+      console.log(err);
+      process.exit(1);
+    }
+  );
