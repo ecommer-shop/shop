c928a35469353829dc06d385ef79a372f805151d
Ronnovic Rigel Toth <t-rrigel@wingo.com>
2025-06-20 23:12:31 -0500

    [main] Add environment variable for STORE_URL and update type definitions; refactor vendure-config for better readability

---
+++ src/enums.ts
@@ -0,0 +1,14 @@
+export enum Route {
+  ADMIN = "admin",
+  ADMIN_API = "admin-api",
+  ASSETS = "assets",
+  MAILBOX = "mailbox",
+  SHOP = "shop",
+  SHOP_API = "shop-api",
+}
+
+export enum RouteStore {
+  verify = "/verify",
+  passwordReset = "/password-reset",
+  changeEmailAddress = "/verify-email-address-change",
+}
