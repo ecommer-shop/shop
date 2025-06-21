f428dbfd5fa330cc93ee3d4f98e8d6ce26c7db1f
Ronnovic Rigel Toth <t-rrigel@wingo.com>
2025-06-21 17:10:28 -0500

    [dev] Add initial data and populate scripts for database seeding, including configuration for payment methods, roles, countries, tax rates, and shipping methods.

---
+++ src/populate/seed.ts
@@ -0,0 +1,26 @@
+import { populate } from '@vendure/core/cli';
+import { bootstrap } from '@vendure/core';
+import { config } from '../vendure-config';
+import path from 'path';
+
+// populate the db with vendure/tests data
+populate(
+  () =>
+    bootstrap({
+      ...config,
+      importExportOptions: {
+        importAssetsDir: path.join(
+          require.resolve('@vendure/create/assets/products.csv'),
+          '../images'
+        ),
+      },
+      dbConnectionOptions: { ...config.dbConnectionOptions, synchronize: true },
+    }),
+  require('@vendure/create/assets/initial-data.json'),
+  require.resolve('@vendure/create/assets/products.csv')
+)
+  .then((app) => app.close())
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
