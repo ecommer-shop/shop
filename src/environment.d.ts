e4f00ffe4a4d3abc279c3fa4509372dcc2e8ace8
williamsevilla <wfernando677@gmail.com>
2025-10-22 23:25:12 -0500

    feat: add SERVIENTREGA_API_KEY to ProcessEnv and clean up environment variables

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -2,30 +2,6 @@ export { };
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv {
-      ADMIN_UI_API_PORT: number;
-      APP_ENV: string;
-      ASSET_UPLOAD_DIR: string;
-      COOKIE_SECRET: string;
-      // DB
-      DB_HOST: string;
-      DB_NAME: string;
-      DB_PASSWORD: string;
-      DB_PORT: number;
-      DB_SCHEMA: string;
-      DB_USERNAME: string;
-      PORT: string;
-      STORE_URL: string;
-      SUPERADMIN_PASSWORD: string;
-      SUPERADMIN_USERNAME: string;
-      STATIC_DIR: string;
-      //PAYMENT
-      PAYMENT_SECRET_KEY: string;
-      //EMAIL
-      RESEND_API_KEY: string;
-      //SERVIENTREGA
-      SERVIENTREGA_BASE: string;
-    }
     interface ProcessEnv { }
   }
 }
