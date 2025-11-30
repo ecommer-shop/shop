777fd87dd0694fd24fc54160fb5e47c0a67e126d
felix0acost <juanfe1663@gmail.com>
2025-11-30 09:38:24 -0500

    feat: agregar plugin de Coinbase a la arquitectura e integrar webhook de Mercado Pago

--- src/config/plugins.ts
+++ src/config/plugins.ts
@@ -22,6 +22,7 @@ import {
 
 import { ROUTE, ROUTE_STORE } from '../consts';
 import { PaymentPlugin } from '../plugins/payment/payment.plugin';
+import { CoinbasePlugin } from "@pinelab/vendure-plugin-coinbase";
 import { CURRENCY } from '../plugins/payment/constants';
 import { Auth0Plugin } from '../plugins/auth0/auth0.plugin';
 import { ServientregaPlugin } from '../plugins/servientrega/servientrega.plugin';
@@ -121,6 +122,7 @@ export const plugins: VendureConfig['plugins'] = [
     },
   }),
 
+  CoinbasePlugin,
   PaymentPlugin.init({
     secretKey: process.env.PAYMENT_SECRET_KEY,
     currency: CURRENCY,
