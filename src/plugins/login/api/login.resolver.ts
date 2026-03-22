c643d95aac3a7b4c4ae1ba9c7f1741c6f2986c25
ElDarwon123 <darwing.j10@gmail.com>
2026-03-21 23:58:12 -0500

    Ft: Update login dashboard env imports

--- src/plugins/login/api/login.resolver.ts
+++ src/plugins/login/api/login.resolver.ts
@@ -1,13 +1,26 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Allow, Ctx, Logger, Permission, RequestContext, Transaction } from '@vendure/core';
 
 import { GoogleAuthService } from '../services/google-auth.service';
 import { loggerCtx } from '../constants';
+import { LoginPlugin } from '../login.plugin';
 import { RegisterSellerWithGoogleInput } from '../types';
 
 @Resolver()
 export class LoginResolver {
     constructor(private googleAuthService: GoogleAuthService) { }
+
+    @Query()
+    @Allow(Permission.Public)
+    loginConfig() {
+        return {
+            googleOAuthClientId:
+                LoginPlugin.options?.googleOAuthClientId ||
+                process.env.GOOGLE_OAUTH_CLIENT_ID ||
+                process.env.VITE_GOOGLE_OAUTH_CLIENT_ID ||
+                '',
+        };
+    }
 
     @Mutation()
     @Transaction()
