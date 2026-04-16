7abe60bac97d8f55397dc801e63bfacbf60061b4
Mary <marymontenegri@unicauca.edu.co>
2026-04-15 23:08:48 -0500

    remove generated admin ui files

--- admin-ui/src/extensions/payment-bank-cert-upload/providers.ts
+++
@@ -1,4 +0,0 @@
-import { registerFormInputComponent } from '@vendure/admin-ui/core';
-import { BankCertPdfUploadComponent } from './bank-cert-pdf-upload.component';
-
-export default [registerFormInputComponent('bank-cert-pdf-upload', BankCertPdfUploadComponent)];
